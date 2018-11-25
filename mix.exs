f57a271e11231dab0f0dd2f6bbf9a24484f20ec0
Adolfo Builes <builes.adolfo@gmail.com>
2018-11-24 20:27:53 -0500

    Prepare for release.

--- mix.exs
+++ mix.exs
@@ -1,10 +1,13 @@
 defmodule BanknotToYnab.MixProject do
   use Mix.Project
 
+  @project_url "https://github.com/abuiles/banknot_to_ynab"
+  @version "0.1.0"
+
   def project do
     [
       app: :banknot_to_ynab,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -20,9 +23,20 @@ defmodule BanknotToYnab.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
+    []
+  end
+
+  defp description() do
+    "Parse notifications from banks outside of America to YNAB transactions."
+  end
+
+   defp package() do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      maintainers: ["Adolfo Builes"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => @project_url
+      }
     ]
   end
 end
