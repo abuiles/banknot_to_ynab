90f645401711d6a459b880260c85d053c93535cb
Adolfo Builes <builes.adolfo@gmail.com>
2019-06-02 10:36:23 -0700

    Bump.

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule BanknotToYnab.MixProject do
   use Mix.Project
 
   @project_url "https://github.com/abuiles/banknot_to_ynab"
-  @version "0.8.0"
+  @version "0.9.0"
 
   def project do
     [
